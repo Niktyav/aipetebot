673760a26550391ca5bd568dcadc87bbef08bf10
Vyatkin.Roman <rvv1976@mail.ru>
2025-01-18 00:02:05 +0300

    update bot

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import telebot
 import os
+import logging
 from telebot import types
 from gigachat import GigaChat
 from gigachat.models import Chat, Messages, MessagesRole
 import re
 import base64
+logging.basicConfig(level=logging.INFO,    
+                    format='%(asctime)s - %(levelname)s - %(module)s - %(message)s'
+                    )
 
+# счетчик для непонимания
+understanding_counter = 0
 # Создаем бота
 bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))
 # Используйте токен, полученный в личном кабинете из поля Авторизационные данные
@@ -20,46 +26,58 @@ payload = Chat(
             content="Ты  утка с большим клювом и зелеными волосами. Ты общаешся как мультяшный выдуманный персонаж. Не любишь очень серьезно рассуждать о чем то, по этому часто подшучиваешь над собеседником."
         )
     ],
-    temperature=0.7,
+    temperature=0.8,
     max_tokens=100,
 )
 
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
+    global understanding_counter
     try:
-        print("User: ", message.text)
-        if ('@aipeteBot' in message.text) or ('крипта' in message.text):
+        logging.info("User: ", message.text)
+
+        if ('@aipeteBot' in message.text) or ('крипта' in message.text): 
             global payload
             payload.messages.append(Messages(role=MessagesRole.USER, content=message.text))
             response = giga.chat(payload)
-            print("Bot: ", response.choices[0].message.content)
-            bot.send_message(message.chat.id, response.choices[0].message.content)
+
+            logging.info("Bot: ", response.choices[0].message.content)
+            bot.send_message(message.chat.id, response.choices[0].message.content)      
+            understang_counter = 0      
         elif 'нарисуй' in message.text.lower() :
             print('start drawing...')
-            payload = Chat(
+            pload = Chat(
                 messages=[Messages(role=MessagesRole.USER, content=message.text)],
                 temperature=0.7,
                 max_tokens=100,
                 function_call="auto",
             )    
-            ans = giga.chat(payload)    
+            ans = giga.chat(pload)    
             # Регулярное выражение для извлечения значения src
             src_match = re.search(r'<img src="([^"]+)"', ans.choices[0].message.content)
+            if src_match is None:
+                logging.warning("Bot cannot draw: ", ans.choices[0].message.content)
+                bot.send_message(message.chat.id, ans.choices[0].message.content)      
+            else:
+                # Регулярное выражение для извлечения подписи (текст после тега <img>)
+                caption_match = re.search(r'/> (.+)', ans.choices[0].message.content)
+                img = giga.get_image(src_match.group(1))
+                print('end drawing...')
 
-            # Регулярное выражение для извлечения подписи (текст после тега <img>)
-            caption_match = re.search(r'/> (.+)', ans.choices[0].message.content)
-            img = giga.get_image(src_match.group(1))
-            print('end drawing...')
+                bot.send_photo(message.chat.id, base64.b64decode(img.content))
+                bot.send_message(message.chat.id, caption_match.group(1))         
+            understanding_counter = 0   
 
-            bot.send_photo(message.chat.id, base64.b64decode(img.content))
-            bot.send_message(message.chat.id, caption_match.group(1))
         else:
-            bot.send_message(message.chat.id, 'моя твоя не понимать обратись ко мне')
+            understanding_counter += 1
+            if understanding_counter > 20:
+                bot.send_message(message.chat.id, 'моя твоя не понимать...')
+                understanding_counter = 0
     except Exception as e:
-        print('Ошибка:',e)
+        logging.error('Ошибка:',e)
 # Запускаем бота
-print('Bot starting...')
+logging.info('Bot starting...')
 
 
 bot.polling(none_stop=True, interval=0)
